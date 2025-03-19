ae7d7cf11dd0d2c13aafddedf53862c45a3802d6
John Lieb <144958513+John13Lieb@users.noreply.github.com>
2025-03-19 18:48:02 -0500

    Added for loop to client to send and receive 7 numbers to and from the server

--- UDPSocket/myFirstUDPClient.java
+++ UDPSocket/myFirstUDPClient.java
@@ -15,75 +15,80 @@ public class myFirstUDPClient {
         if (args.length != 2) // Test for correct # of args
             throw new IllegalArgumentException("Parameter(s): <Server> [<Port>]");
 
-        InetAddress serverAddress = InetAddress.getByName(args[0]);  // Server address
-        // Convert input String to bytes using the default character encoding
+        double[] runTimes = new double[7]; // Array to store round-trip times
         Scanner scanner = new Scanner(System.in);
+        String[] runs = {"first", "second", "third", "fourth", "fifth", "sixth", "seventh"};
 
-        byte[] bytesToSend = new byte[2];
-        System.out.print("Enter short in the range (-32,768 to 32,767): ");
+        // Convert input String to bytes using the default character encoding
+        for (int i = 0; i < 7; i++) {
+            byte[] bytesToSend = new byte[2];
+            System.out.print("Enter " + runs[i] + " short in the range (-32,768 to 32,767): ");
 
-        short val;
-        while (true) {
-            try {
-                val = scanner.nextShort(); // Read short from user
-                break;
-            } catch (Exception e) {
-                System.out.print("Invalid input. Please enter a short value in the range (-32,768 to 32,767): ");
-                scanner.next(); // Clear invalid input
+            short val;
+            while (true) {
+                try {
+                    val = scanner.nextShort(); // Read short from user
+                    break;
+                } catch (Exception e) {
+                    System.out.print("Invalid input. Please enter a short value in the range (-32,768 to 32,767): ");
+                    scanner.next(); // Clear invalid input
+                }
             }
-        }
 
-        ByteBuffer.wrap(bytesToSend).order(ByteOrder.BIG_ENDIAN).putShort(val); // convert short to byte array
-        System.out.print("\nSending Bytes: ");
-        for (byte b : bytesToSend) {
-            System.out.printf("0x%02X ", b);
-        }
-        System.out.println();
-
-        int servPort = Integer.parseInt(args[1]);
-
-        DatagramSocket socket = new DatagramSocket();
-
-        socket.setSoTimeout(TIMEOUT);  // Maximum receive blocking time (milliseconds)
-
-        DatagramPacket sendPacket = new DatagramPacket(bytesToSend,  // Sending packet
-                bytesToSend.length, serverAddress, servPort);
-
-        String numStr = Short.toString(val);
-        int recvMsgSize = numStr.length() * 2 + 2; // Size of received message (UTF-16)
-
-        DatagramPacket receivePacket = new DatagramPacket(new byte[recvMsgSize], recvMsgSize); // Receiving packet
-
-        int tries = 0;      // Packets may be lost, so we have to keep trying
-        boolean receivedResponse = false;
-        do {
-            socket.send(sendPacket);          // Send the echo string
-            try {
-                socket.receive(receivePacket);  // Attempt echo reply reception
-
-                if (!receivePacket.getAddress().equals(serverAddress))  // Check source
-                    throw new IOException("Received packet from an unknown source");
-
-                receivedResponse = true;
-            } catch (InterruptedIOException e) {  // We did not get anything
-                tries += 1;
-                System.out.println("Timed out, " + (MAXTRIES-tries) + " more tries...");
+            ByteBuffer.wrap(bytesToSend).order(ByteOrder.BIG_ENDIAN).putShort(val); // convert short to byte array
+            System.out.print("\nSending Bytes: ");
+            for (byte b : bytesToSend) {
+                System.out.printf("0x%02X ", b);
             }
-        } while ((!receivedResponse) && (tries < MAXTRIES));
+            System.out.println();
 
-        if (receivedResponse) {
-            byte[] recvBuffer = receivePacket.getData();
+            InetAddress serverAddress = InetAddress.getByName(args[0]);  // Server name or IP address
+            int servPort = Integer.parseInt(args[1]);
 
-            System.out.print("Received Bytes: ");
-            for (byte r : recvBuffer) {
-                System.out.printf("0x%02X ", r);
+            DatagramSocket socket = new DatagramSocket();
+
+            socket.setSoTimeout(TIMEOUT);  // Maximum receive blocking time (milliseconds)
+
+            DatagramPacket sendPacket = new DatagramPacket(bytesToSend,  // Sending packet
+                    bytesToSend.length, serverAddress, servPort);
+
+            String numStr = Short.toString(val);
+            int recvMsgSize = numStr.length() * 2 + 2; // Size of received message (UTF-16)
+
+            DatagramPacket receivePacket = new DatagramPacket(new byte[recvMsgSize], recvMsgSize); // Receiving packet
+
+            int tries = 0;      // Packets may be lost, so we have to keep trying
+            boolean receivedResponse = false;
+            do {
+                socket.send(sendPacket);          // Send the echo string
+                try {
+                    socket.receive(receivePacket);  // Attempt echo reply reception
+
+                    if (!receivePacket.getAddress().equals(serverAddress))  // Check source
+                        throw new IOException("Received packet from an unknown source");
+
+                    receivedResponse = true;
+                } catch (InterruptedIOException e) {  // We did not get anything
+                    tries += 1;
+                    System.out.println("Timed out, " + (MAXTRIES - tries) + " more tries...");
+                }
+            } while ((!receivedResponse) && (tries < MAXTRIES));
+
+            if (receivedResponse) {
+                byte[] recvBuffer = receivePacket.getData();
+
+                System.out.print("Received Bytes: ");
+                for (byte r : recvBuffer) {
+                    System.out.printf("0x%02X ", r);
+                }
+                System.out.println("\n");
+            } else {
+                System.out.println("No response -- giving up.");
             }
-            System.out.println("\n");
-        }
-        else {
-            System.out.println("No response -- giving up.");
-        }
 
-        socket.close();
+            socket.close();
+        }
+        scanner.close();
+
     }
 }
